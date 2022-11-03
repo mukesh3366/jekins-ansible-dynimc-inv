bb126e89905d7243a5bd9ae5db12da6a99809192
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-03 21:34:41 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -2,7 +2,11 @@ provider "aws"{
    region  = "ap-southeast-1" 
    
 }
-
+resource "aws_vpc" "vpc" {
+  cidr_block                       = "10.0.0.0/16"
+  instance_tenancy                 = "default"
+  
+}
 resource "aws_instance" "jenkinswithterraform"{
      count   = 1
      ami     =  "ami-051f0947e420652a9"
@@ -13,8 +17,4 @@ resource "aws_instance" "jenkinswithterraform"{
         Name = "tomcatserver"
      }
 }
-resource "aws_vpc" "vpc" {
-  cidr_block                       = "10.0.0.0/16" 
-  instance_tenancy                 = "default"
-  
-}
+
