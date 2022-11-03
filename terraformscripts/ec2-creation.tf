3d02dcb949dc8198ad817cf05a2100685b5c6c3f
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-03 18:44:04 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -3,14 +3,13 @@ provider "aws"{
    
 }
 
-resource "aws_instance" "AWSEC2Instance"{
+resource "aws_instance" "jenkinswithterraform"{
      count   = 1
-     ami     =  "ami-051f0947e420652a9"
+     ami     =  "ami-05c8ca4485f8b138a"
      instance_type = "t2.micro"
      security_groups = ["launch-wizard-2"]
-     key_name        = "devopsmss"
+     key_name        = "project"
      tags = {
         Name = "tomcatserver"
      }
 }
-
