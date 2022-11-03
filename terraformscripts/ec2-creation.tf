b33c53ea0f39b5b0d3492fb36da89e911890323e
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-03 21:31:41 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "jenkinswithterraform"{
      }
 }
 resource "aws_vpc" "vpc" {
-  cidr_block                       = 10.0.0.0/16
+  cidr_block                       = "10.0.0.0/16" 
   instance_tenancy                 = "default"
   
 }
