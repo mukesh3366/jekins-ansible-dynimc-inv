dcf659a83357027b2236910fdbfb205793c51cb1
mukesh3366 <111684148+mukesh3366@users.noreply.github.com>
2022-11-04 11:16:10 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,11 +1,11 @@
 provider "aws"{
-   region  = "ap-southeast-1" 
+   region  = "ap-south-1" 
    
 }
 
 
-resource "aws_instance" "tomcatserver" {
-  ami           = "ami-051f0947e420652a9"
+resource "aws_instance" "jenkinswithterraform" {
+  ami           = "ami-05c8ca4485f8b138a"
   instance_type = "t2.micro"
 
   tags = {
